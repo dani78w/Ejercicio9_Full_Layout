4a7c228de4d824049a750dc23630d523d986c2f7
daniel <danielarribas78w@gmail.com>
2023-02-02 23:26:52 +0100

    Interfaces

---
+++ app/src/main/java/com/example/ejercicio9_full_layout/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.ejercicio9_full_layout
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.ejercicio9_full_layout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /*
+        Esquinas redondeadas
+        https://developer.android.com/develop/ui/views/theming/rounded-corners
+         */
+
+    }
+}
