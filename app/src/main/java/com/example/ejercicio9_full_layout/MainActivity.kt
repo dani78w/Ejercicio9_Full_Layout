a78a4d9a74435dd6c80ab94f2fa6112f27d314b9
lcx06 <lucian160403@gmail.com>
2023-02-05 21:41:28 +0100

    Cleanup and organization

--- app/src/main/java/com/example/ejercicio9_full_layout/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio9_full_layout/MainActivity.kt
@@ -1,75 +1,72 @@
 package com.example.ejercicio9_full_layout
 
 import android.content.Intent
-import android.content.IntentSender
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.ejercicio9_full_layout.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        val telIntent: Intent = Uri.parse("tel:666666666").let { number ->
-            Intent(Intent.ACTION_DIAL, number)
-        }
-        val gmailIntent: Intent = Uri.parse("https://mail.google.com/mail/").let { webpage ->
-            Intent(Intent.ACTION_VIEW, webpage)
-        }
-        val geoIntent: Intent = Uri.parse("geo:52.36524017156559,4.926453531223001").let { location ->
-            Intent(Intent.ACTION_VIEW, location)
-        }
-        val mapsIntent: Intent = Uri.parse("https://maps.google.com/").let { webpage ->
-            Intent(Intent.ACTION_VIEW, webpage)
-        }
-        val hangoutsIntent: Intent = Uri.parse("https://hangouts.google.com/").let {webpage ->
-            Intent(Intent.ACTION_VIEW, webpage)
-        }
-        val hangoutsAppIntent: Intent = Uri.parse("com.google.hangouts://").let {app ->
-            Intent(Intent.ACTION_VIEW,app)
-        }
         binding.mapsButton.setOnClickListener {
-            startActivity(mapsIntent)
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.google.com/")))
         }
+
         binding.hangoutsButton.setOnClickListener {
+            // La URI de Hangouts es "com.google.hangouts://" pero si no tiene la app instalada manejamos la excepción y abrimos la web
             try {
-                startActivity(hangoutsAppIntent)
-            }catch (e: java.lang.Exception){
-                startActivity(hangoutsIntent)
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("com.google.hangouts://")
+                    )
+                )
+            } catch (e: java.lang.Exception) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://hangouts.google.com/")
+                    )
+                )
+            }
+        }
+
+        val mailAction: (View) -> Unit = {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://mail.google.com/mail/")
+                    )
+                )
             }
 
-        }
-        binding.gmailButton.setOnClickListener {
-            startActivity(gmailIntent)
-        }
+        binding.gmailButton.setOnClickListener(mailAction)
+        binding.thirdCard.setOnClickListener(mailAction)
+
         binding.firstCard.setOnClickListener {
-            startActivity(geoIntent)
+            // Para abrir la app de Maps con una localización concreta, se le pasa la URI "geo:latitud,longitud"
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("geo:52.36524017156559,4.926453531223001")
+                )
+            )
         }
+
         binding.secondCard.setOnClickListener {
-            startActivity(telIntent)
+            // Para abrir la app de llamadas, se le pasa la URI "tel:numero" con la acción ACTION_DIAL
+            startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:666666666")))
         }
 
-
-        /*
-        Esquinas redondeadas
-        https://developer.android.com/develop/ui/views/theming/rounded-corners
-         */
-
+        binding.voiceIcon.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VOICE_COMMAND)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        }
     }
 }
